92904adc692efe4c4171300d2038a0d9e760b706
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-07-07 15:50:58 +0530

    dsa pratical linkdinlist

---
+++ DSA/pratical/linkdnlist.py
@@ -0,0 +1,67 @@
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Linkdilist:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_begnning(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_ending(self,data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def serch(self,key):
+        if not self.head:
+            print('list is empty')
+            return 
+        current = self.head
+        while current : 
+            if current.data == key:
+               return True
+            current = current.next
+           
+        return False
+        
+    def delete(self,key):
+        current = self.head
+        prev = None
+        while current and current.data != key :
+            prev = current
+            current = current.next
+        prev.next = current.next
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data,end=' -> ')
+            current = current.next
+        print('none')
+
+li= Linkdilist()
+li.insert_at_begnning(10)    
+li.insert_at_begnning(20)    
+li.insert_at_begnning(30)    
+li.insert_at_ending(40)
+li.insert_at_ending(50)
+li.insert_at_ending(60)
+print(li.serch(60))
+li.delete(60)
+print(li.serch(60))
+
+li.print_list()
+             
+        
