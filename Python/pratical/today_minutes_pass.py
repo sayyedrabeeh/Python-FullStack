e70227ed0e6bd79af19719ab5140b5337c1d42f4
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-10-06 19:16:07 +0530

    minute passed

---
+++ Python/pratical/today_minutes_pass.py
@@ -0,0 +1,7 @@
+from datetime import datetime,time
+
+now1 = datetime.now()
+midnight = datetime.combine(now1.date(),time(0,0))
+minute_pased = (now1 -midnight ).total_seconds() // 60
+
+print(minute_passed)
