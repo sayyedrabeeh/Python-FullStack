28793856fe80bce5014896778ff7102405c7ce00
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-12-01 15:47:26 +0530

    treen  with n childrens controlling the number of child

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
+a = [1,2,3]
+b = [4,5]
+a.append(b)
+print(a)
 
-
-
-s = "he12l5lo90"
-num = ''
-result = []
-for i in s:
-    if i.isdigit():
-        num += i
-    else:
-        if num:
-            result.append(num)
-            num = ''
-if num:
-    result.append(num)
-
-print(' '.join(result))
+a = [1,2,3]
+a.extend(b)
+print(a)
